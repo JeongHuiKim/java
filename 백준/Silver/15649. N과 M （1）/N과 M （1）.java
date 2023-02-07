4ffa57846da0b4632bc66c8122ffc13ad73c78a9
jeonghui <87287223+JeongHuiKim@users.noreply.github.com>
2023-02-07 13:46:44 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 34552 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
+
+	static int num[];
+	static boolean visit[];
+	static int ans[];
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		boolean[] visit = new boolean[N];
-		int[] contain = new int[M];
-		dfs(visit,contain, N, M, 0);
-	
+		ans = new int[M];
+		num = new int[N];
+		for(int i = 0 ; i < N ; i++) {
+			num[i] = i+1;
+		}
+		visit = new boolean[N];
+		
+		permute(0);
+		
+		System.out.println(sb);
+		
 	}
 	
-	static void dfs(boolean[] visit, int[] contain, int N , int M, int count) {
+	static void permute(int cnt) {
 		
-		if(count == M) {
-			for(int con : contain) {
-				System.out.print( (con+1) + " " );
+		// 기저 조건
+		if(cnt == M) {
+			for(int i = 0 ; i < M ; i++) {
+				sb.append(ans[i] + " ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
 		
 		for(int i = 0 ; i < N ; i++) {
-			if(!visit[i]) {
-				visit[i] = true;
-				contain[count] = i;
-				dfs(visit, contain, N, M, count + 1);
-				visit[i] = false;
-			}
+			
+			if(visit[i]) continue;
+			
+			ans[cnt] = num[i];
+			visit[i] = true;
+			permute(cnt+1);
+			visit[i] = false;
 			
 		}
 		
