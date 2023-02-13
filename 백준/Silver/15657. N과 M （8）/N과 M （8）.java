d82c36039f5f80666653efe881896b20e8aede7d
jeonghui <87287223+JeongHuiKim@users.noreply.github.com>
2023-02-13 16:51:49 +0900

    [Silver III] Title: N과 M (8), Time: 212 ms, Memory: 21100 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static boolean[] isSelected;
+	static int[] input;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isSelected = new boolean[N];
+		ans = new int[M];
+		input = new int[N];
+
+		st = new StringTokenizer(in.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(input);
+
+		combi(0, 0);
+
+		System.out.print(sb);
+	}
+
+	static void combi(int cnt, int start) {
+		if(cnt == M) {
+			for(int i = 0 ; i < M ; i++) {
+				sb.append(ans[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for(int i = start ; i < N ; i++) {
+			ans[cnt] = input[i];
+			isSelected[i] = true;
+			combi(cnt+1, i);
+			isSelected[i] = false;
+		}
+
+	}
+}
