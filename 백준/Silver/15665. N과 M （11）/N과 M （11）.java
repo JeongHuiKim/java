559a9def2f8bb7db887ece0647df8690f6ab549a
jeonghui <87287223+JeongHuiKim@users.noreply.github.com>
2023-02-14 08:37:41 +0900

    [Silver II] Title: N과 M (11), Time: 1644 ms, Memory: 355576 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static boolean[] isSelected;
+	static int[] input;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
+	
+	static HashSet<String> set = new HashSet<>();
+
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isSelected = new boolean[N];
+		ans = new int[M];
+		input = new int[N];
+
+		st = new StringTokenizer(in.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(input);
+
+		combi(0);
+
+		System.out.println(sb);
+	}
+
+	static void combi(int cnt) {
+		if(cnt == M) {
+			String str = "";
+			for(int i = 0 ; i < M ; i++) {
+				str += String.valueOf(ans[i]);
+				str += " ";
+//				sb.append(ans[i] + " ");
+			}
+			if(!set.contains(str)) {
+				set.add(str);
+				sb.append(str + "\n");
+			}
+			return;
+		}
+
+		for(int i = 0 ; i < N ; i++) {
+				ans[cnt] = input[i];
+				isSelected[i] = true;
+				combi(cnt+1);
+				isSelected[i] = false;
+		}
+
+	}
+}
